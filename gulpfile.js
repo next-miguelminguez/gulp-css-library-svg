7ee801ea941e5e1a66130be55d3852a78da02f68
Miguel Minguez <m.minguezz@gmail.com>
2018-11-15 10:44:29 +0100

    (new) add files

--- gulpfile.js
+++ gulpfile.js
@@ -10,12 +10,44 @@ var path     = require('path');
 var svgmin   = require('gulp-svgmin');
 var svgo     = require('gulp-svgo');
 var replace  = require('gulp-replace');
+var rename   = require("gulp-rename");
 
-
-gulp.task('default', function () {
+gulp.task('svg', function () {
   return gulp
     .src( config.src + '/*.svg' )
-    // .pipe(svgo())
-    //.pipe(svgmin())
     .pipe( gulp.dest(config.dest) );
 });
+gulp.task('svgo', function () {
+  return gulp
+    .src( config.src + '/*.svg' )
+    .pipe(svgo())
+    .pipe( rename({
+      suffix: "--svgo",
+      extname: ".svg"
+    }) )
+    .pipe( gulp.dest(config.dest) );
+});
+gulp.task('svgmin', function () {
+  return gulp
+    .src( config.src + '/*.svg' )
+    .pipe( replace(' id=', ' class=') )
+    .pipe( replace('<?','<?xml-stylesheet type="text/css" href="../../src/init.css"?><?') )
+    .pipe( svgmin({
+      js2svg: {
+        pretty: true
+      },
+      plugins: [
+        {removeDimensions:    true},
+        {removeViewBox:       false},
+        {convertStyleToAttrs: false}
+      ]
+    }) )
+    .pipe( rename({
+      suffix: "--svgmin",
+      extname: ".svg"
+    }) )
+    .pipe( gulp.dest(config.dest) );
+});
+
+// gulp.task('default', ['svg', 'svgo', 'svgmin'] );
+gulp.task('default', [ 'svgmin'] );
--- thesaurus.md
+++ thesaurus.md
@@ -1,7 +1,7 @@
 # Diccionario
 
 * item__background: El fondo del elemento
-* item__content: El contenido del elemento
-* item--fill: Elemento que cambia el color de relleno
-* item--shadow: Elemento que tiene sombra
-* item__animate--letter: anima los hijos 
+* item__content:    El contenido del elemento
+* item--fill:       Elemento que cambia el color de relleno
+* item--shadow:     Elemento que tiene sombra
+* item__animate--letter: Anima los hijos 
