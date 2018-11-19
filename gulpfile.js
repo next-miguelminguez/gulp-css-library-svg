77b1e64f1e4d4a02825c65304fc4b04b2af6ab01
Miguel Minguez <m.minguezz@gmail.com>
2018-11-19 14:53:56 +0100

    (new) BBVA example

--- gulpfile.js
+++ gulpfile.js
@@ -7,47 +7,83 @@ var config   = require('./config.json');
 var gulp     = require('gulp');
 
 var path     = require('path');
+var fs       = require('fs');
+
 var svgmin   = require('gulp-svgmin');
-var svgo     = require('gulp-svgo');
 var replace  = require('gulp-replace');
 var rename   = require("gulp-rename");
 
-gulp.task('svg', function () {
-  return gulp
-    .src( config.src + '/*.svg' )
-    .pipe( gulp.dest(config.dest) );
-});
-gulp.task('svgo', function () {
-  return gulp
-    .src( config.src + '/*.svg' )
-    .pipe(svgo())
-    .pipe( rename({
-      suffix: "--svgo",
-      extname: ".svg"
-    }) )
-    .pipe( gulp.dest(config.dest) );
-});
+
 gulp.task('svgmin', function () {
   return gulp
     .src( config.src + '/*.svg' )
+
     .pipe( replace(' id=', ' class=') )
-    .pipe( replace('<?','<?xml-stylesheet type="text/css" href="../../src/init.css"?><?') )
+    //.pipe( replace('<?','<?xml-stylesheet type="text/css" href="../../src/init.css"?><?') )/* alternative CSS */
     .pipe( svgmin({
-      js2svg: {
-        pretty: true
-      },
+      // js2svg: {
+      //   pretty: true
+      // },
       plugins: [
-        {removeDimensions:    true},
-        {removeViewBox:       false},
-        {convertStyleToAttrs: false}
+        { removeDimensions:      true  },
+        { removeViewBox:         false },
+        { convertStyleToAttrs:   false }
       ]
     }) )
+    // .pipe( replace('<!--Generator: Sketch 52.4 (67378) - http://www.bohemiancoding.com/sketch-->', clip))
     .pipe( rename({
-      suffix: "--svgmin",
+      suffix: "-min",
       extname: ".svg"
     }) )
-    .pipe( gulp.dest(config.dest) );
+    .pipe( gulp.dest(config.dest+"/svg/") );
 });
 
-// gulp.task('default', ['svg', 'svgo', 'svgmin'] );
-gulp.task('default', [ 'svgmin'] );
+gulp.task('copy-css', function(){
+  gulp.src(['./src/css/**/*']).pipe(gulp.dest(config.dest + '/css/'));
+});
+gulp.task('demo', ['svgmin'], function () {
+  var svgContent = fs.readFileSync(config.dest +"/svg/"+ "next-svg" +'-min.svg', "utf8");
+  var svgMicro = fs.readFileSync(config.dest +"/svg/"+ "micro" +'-min.svg', "utf8");
+  
+  return gulp
+    .src( config.demo )
+    .pipe( replace('<!-- micro -->', svgMicro) )
+    .pipe( replace('<!-- replace -->', svgContent) )
+    .pipe( gulp.dest(config.dest + "/demo/") );
+});
+
+gulp.task('usecase', ['svgmin'], function () {
+
+
+  return gulp
+    .src( config.demo )
+
+    .pipe( gulp.dest(config.dest + "/demo/") );
+});
+
+
+gulp.task('default', ['demo', 'copy-css'] );
+
+
+
+// Atajo: se crea en el archivo clip.svg una forma o mascara que se inserta dentro del SVG en el comentario.
+// Con CSS asignamos la mascara al elemento del SVG que nos interese.
+//
+// Ejemplos:
+//  * basic https://css-tricks.com/clipping-masking-css/
+//  * top  https://codepen.io/yoksel/full/fsdbu/
+//
+// var clip = fs.readFileSync('./src/assets/clip.svg', "utf8");
+//
+// gulp.task('svg-mask', function () {
+//   return gulp
+//     .src( config.src + '/*.svg' )
+//     .pipe( replace('<!-- Generator: Sketch 52.4 (67378) - http://www.bohemiancoding.com/sketch -->', clip))
+//     .pipe( gulp.dest(config.dest) );
+// });
+
+// Alternative para incluir css
+// <defs>
+//   <link href="../../src/init.css" type="text/css" rel="stylesheet"
+//           xmlns="http://www.w3.org/1999/xhtml"/>
+// </defs>
--- thesaurus.md
+++ thesaurus.md
@@ -1,7 +1,25 @@
 # Diccionario
 
-* item__background: El fondo del elemento
-* item__content:    El contenido del elemento
-* item--fill:       Elemento que cambia el color de relleno
-* item--shadow:     Elemento que tiene sombra
-* item__animate--letter: Anima los hijos 
+* nxt-svg__bg:         Grupo de elemento de fondo de la imagen
+* nxt-svg__content:    Grupo de elemento de contenido de la imagen
+* nxt-svg__item:       Elemento a modificar en la imagen
+* nxt-svg--anim:       Grupo de elementos que pueden ser animados
+
+* nxt-svg__fill:         Clase que cambia el color de relleno
+* nxt-svg__stroke:       Clase que cambia el color del borde y quita relleno
+* nxt-svg--shadow:       Clase que tiene sombra
+* nxt-svg__goldstar:     Clase que aplica un estilo
+
+* nxt-svg__bg--circle:            Clase que cambia el fondo a circulo
+* nxt-svg__bg--round:             Clase que cambia el fondo a esquinas redondeadas
+* nxt-svg__bg--polygon-next:      Clase que cambia el fondo a polígono
+* nxt-svg__bg--polygon-message:   Clase que cambia el fondo a polígono
+
+* nxt-svg__bg--triangle:          Clase que cambia el fondo a triangulo
+* nxt-svg__bg--polygon-inset:     Clase que cambia el fondo a polígono
+
+* nxt-svg__anim--hideshow:        Clase que anima el contenido
+* nxt-svg__anim--kaboom:          Clase que anima el contenido
+* nxt-svg__anim--bounceInRight:   Clase que anima el contenido
+* nxt-svg__anim--rotate-center:   Clase que anima el contenido
+* nxt-svg__bg--clip-animation:    Clase que modifica y anima el fondo
